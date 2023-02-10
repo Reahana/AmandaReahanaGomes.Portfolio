360133d70b91cb53b844cd8f58937f3d116de84e
Amanda Reahana Gomes <67775406+Reahana@users.noreply.github.com>
2023-02-10 13:10:33 +0600

    lecturer

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Web Developer","Web Designer","Website Tester","Contant Creator","Rotaractor"],
+        strings: [ "Web Developer","Web Designer","Website Tester","Contant Creator","Lecturer","Rotaractor"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
